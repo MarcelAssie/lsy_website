c61067d722ad533d0744a4856275508e0e43d3bc
Marcel Assie <marcelassie2k@gmail.com>
2024-07-24 03:57:18 +0200

    modification du fichier requirements.txt

--- lsy_website/settings.py
+++ lsy_website/settings.py
@@ -19,7 +19,7 @@ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-environ.Env.read.env()
+env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -128,7 +128,6 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
