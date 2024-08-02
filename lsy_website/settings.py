9986c35e12f74e92fc8fece7c1a28e1655ef568a
Marcel Assie <marcelassie2k@gmail.com>
2024-08-02 23:59:20 +0200

    delecting local environments settings 5555

--- lsy_website/settings.py
+++ lsy_website/settings.py
@@ -28,9 +28,26 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-
+"""
 # LOCAL ENVIRONMENT SETTINGS
+SECRET_KEY = 'django-insecure-k0th%jqik@rn*e@$hoqpwfpe_jj)rv4=3$$l3$w68&x!8a2&8b'
+DEBUG = True
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'marceldevtest@gmail.com'
+EMAIL_HOST_PASSWORD = 'fszomdfgibjvkjya'
+CONTACT_EMAIL = ['josemarcelassie@gmail.com', 'marceldevtest@gmail.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
 
 
 # DEPLOYMENT ENVIRONMENT SETTINGS
@@ -133,10 +150,11 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'marceldevtest@gmail.com'
 EMAIL_HOST_PASSWORD = 'fszomdfgibjvkjya'
-
+CONTACT_EMAIL = ['josemarcelassie@gmail.com', 'marceldevtest@gmail.com']
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
