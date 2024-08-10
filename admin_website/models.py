bcc71c9993950d6b8203ee3b23ac924547c3fab3
Marcel Assie <marcelassie2k@gmail.com>
2024-08-10 14:15:18 +0200

    Modifs du noms des fichiers 2222

--- admin_website/models.py
+++ admin_website/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 from django.utils import timezone
+import os
+def upload_to_actualites(instance, filename):
+    return os.path.join('actualites/', filename)
+
+def upload_to_evenements(instance, filename):
+    return os.path.join('evenements/', filename)
+
+def upload_to_annales(instance, filename):
+    return os.path.join('annales/', filename)
+
+def upload_to_temoignages(instance, filename):
+    return os.path.join('temoignages/', filename)
+
 class Actualite(models.Model):
     titre = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField(upload_to='actualites/')
+    image = models.ImageField(upload_to=upload_to_actualites)
     date_publiée = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
@@ -12,27 +25,25 @@ class Actualite(models.Model):
 class Evenement(models.Model):
     titre = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField(upload_to='evenements/')
+    image = models.ImageField(upload_to=upload_to_evenements)
     date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.titre
 
-
 class Annale(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    file = models.FileField(upload_to='annales/')
+    file = models.FileField(upload_to=upload_to_annales)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
 
-
 class Testimonial(models.Model):
     name = models.CharField(max_length=100, verbose_name="Nom")
     promotion = models.CharField(max_length=50, verbose_name="Promotion")
-    photo = models.ImageField(upload_to='temoignages/', blank=True, null=True, verbose_name="Photo")
+    photo = models.ImageField(upload_to=upload_to_temoignages, blank=True, null=True, verbose_name="Photo")
     text = models.TextField(verbose_name="Texte du Témoignage")
 
     def __str__(self):
